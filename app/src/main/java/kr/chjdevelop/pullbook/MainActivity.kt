0ea7691600f79ac4b263f8755be4c25631b88482
CHJUNE-android <chjune0205@gmail.com>
2019-12-13 22:40:38 +0900

    read_book

--- app/src/main/java/kr/chjdevelop/pullbook/MainActivity.kt
+++ app/src/main/java/kr/chjdevelop/pullbook/MainActivity.kt
@@ -1,12 +1,21 @@
 package kr.chjdevelop.pullbook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+    }
+    private fun init(){
+        rl_read_book.setOnClickListener{
+            val intent = Intent(this,ReadBookActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
